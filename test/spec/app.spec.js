ac6dc426987b06c0f3cd7490076fefd0ee8cc7f8
M4rc0SX <federicimarcosx@gmail.com>
2015-12-15 17:11:54 +0100

    Jasmine testing finito

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,10 +1,33 @@
 /* Remember that blanket will only work with brackets live preview */
 /* Try to maximise the coverage of the SantaModel object */
 
-describe("Santa", function() {
+describe("Santa", function () {
 
-  it("should...", function() {  
-    
-  });    
- 
+    it("should give the first item of the list", function () {
+        SantaModel.init([1,2,3,4]);
+        expect(SantaModel.getCurrentRequest()).toBe(1);
+    });
+
+    it("should give the second item of the list after calling the function next()", function () {
+        SantaModel.init([1,2,3,4]);
+        SantaModel.next();
+        expect(SantaModel.getCurrentRequest()).toBe(2);
+    });
+
+    it("should give undefined if the list is end", function () {
+        SantaModel.init([1]);
+        SantaModel.next();
+        expect(SantaModel.getCurrentRequest()).toBeUndefined();
+    });
+
+    it("should tell if the pack is correct", function () {
+        SantaModel.init([{answer:1}]);
+        expect(SantaModel.pack(1)).toBe(1);
+    });
+
+    it("should tell if the pack is wrong", function () {
+        SantaModel.init([{answer:1}]);
+        expect(SantaModel.pack(2)).toBe(0);
+    });
+
 });
